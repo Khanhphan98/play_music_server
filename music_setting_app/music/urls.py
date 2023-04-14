f7ad92a85804f8fa88355da1560b43e787fbaeca
Khanhphan98 <khanhphan9898@gmail.com>
2023-04-14 10:37:46 +0700

    thêm refresh token

--- music_setting_app/music/urls.py
+++ music_setting_app/music/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-urlpatterns = []
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
 
 # View set
 router = DefaultRouter()
@@ -11,4 +15,6 @@ router.register(r'singer', SingerViewSet)
 router.register(r'category', CategoryViewSet)
 router.register(r'country', CountryViewSet)
 router.register(r'song', SongViewSet)
+
+
 urlpatterns += router.urls
